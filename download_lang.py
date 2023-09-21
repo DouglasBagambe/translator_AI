32dcb8ecbd3ebcd6c1bfa869b6ba08f3c692134d
Douglas Bagambe <108017894+DouglasBagambe@users.noreply.github.com>
2023-09-22 00:09:11 +0300

    Add files via upload

---
+++ download_lang.py
@@ -0,0 +1,39 @@
+import os
+
+# # Read the languages from the file
+# with open("languages.txt") as f:
+#     languages = f.read().splitlines()
+
+# # Download each language model
+# for lang in languages:
+#     command = f"python -m spacy download {lang}"
+#     os.system(command)
+#     print(f"Downloaded language model: {lang}")
+    
+import spacy
+import subprocess
+
+# List of spaCy model names
+models = [
+    'en_core_web_sm',
+    'es_core_news_sm',
+    'fr_core_news_sm',
+    'de_core_news_sm',
+    'zh_core_web_sm',
+    'xx_ent_wiki_sm',
+    'es_core_news_md',
+    'pt_core_news_sm',
+    'ja_core_news_sm'
+]
+
+# Iterate through the models and download them
+for model_name in models:
+    try:
+        # Use subprocess to run the Homebrew Python and the spaCy download command
+        subprocess.run(['/opt/homebrew/bin/python3', '-m', 'spacy', 'download', model_name])
+        print(f"Downloaded language model: {model_name}")
+    except Exception as e:
+        print(f"Error downloading {model_name}: {str(e)}")
+
+print("All models downloaded successfully.")
+    
